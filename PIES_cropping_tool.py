e91234100529b3f5957b1741d526b3acb884b241
hhelmick92 <harrison.helmick@gmail.com>
2024-06-01 13:14:41 -0500

    Update PIES_cropping_tool.py

--- PIES_cropping_tool.py
+++ PIES_cropping_tool.py
@@ -18,7 +18,7 @@ import time as count_time
 # In[]
 
 folders = list()
-for root, dirs, files in os.walk(r"L:\Kokini Lab\Kara Benbow\pie_crust_pictures_v2", topdown=False):
+for root, dirs, files in os.walk(r"PATH\TO\THE\IMAGES\TO\PROCESS", topdown=False):
     for name in dirs:
         folders.append(os.path.join(root, name))
 
@@ -93,8 +93,8 @@ start_time = count_time.time()
 # WRITE OUT THE CROPPED IMAGES FOR EASIER FUTURE ANALYSIS. CROPPING IS DONE PER TIME BASED ON THRESHOLD VALUES
 write_out_backs_pies(
     imgs_to_grab = t15[0],
-    put_backs_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t15\backs',
-    put_pies_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t15\pies',
+    put_backs_here = r'PATH\TO\WHERE\TO\SAVE\DATA\BACKGROUND',
+    put_pies_here = r'PATH\TO\WHERE\TO\SAVE\DATA\CROPPED_IMAGES',
     treshold_in = 75
     )
 
@@ -102,46 +102,8 @@ elap = count_time.time() - start_time
 # for full data set, elap = 5.166666667
 
 # In[]
-'''
-failed = glob.glob(r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t15\failed_75\*.png')
 
-failed_names = []
-for fn in failed:
-    t1 = (fn.split('\\')[-1][-8:])
-    #failed_names.append(t1)
-    failed_names.append(t1.lower().replace('.png', '.jpg'))
-
-t15_failed = []
-for f in t15[0]:
-    f1 = f.split('\\')
-    f2 = (f1[-1]).lower()
-    for n in failed_names:
-        if n in f2:
-            t15_failed.append(f)
-
-l1 = [
-      'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph3_time15_weight1\\IMG_3592.JPG',
-       'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph8_time15_weight1\\IMG_3431.JPG',
-       'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea80_ph8_time15_weight4\\IMG_3446.JPG',
-       'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea90_ph8_time15_weight1\\IMG_3424.JPG',
-       'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea90_ph8_time15_weight1\\IMG_3425.JPG',
-       'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea90_ph8_time15_weight2\\IMG_3439.JPG'
-      ]
-
-# In[]
-
-# WRITE OUT THE CROPPED IMAGES FOR EASIER FUTURE ANALYSIS. CROPPING IS DONE PER TIME BASED ON THRESHOLD VALUES
-write_out_backs_pies(
-    imgs_to_grab = l1,
-    put_backs_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t15\thresh100\backs',
-    put_pies_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t15\thresh100\pies',
-    treshold_in = 150
-    )
-'''
-
-# In[]
-
-done = glob.glob(r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t30_1\pies\*.png')
+done = glob.glob(r'PATH\TO\CROPPED\IMAGES\*.png')
 
 done_names = []
 for fn in done:
@@ -169,57 +131,13 @@ len(not_done) + len(done) == len(t30[0])
 start_time = count_time.time()
 write_out_backs_pies(
     imgs_to_grab = not_done,
-    put_backs_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t30_2\backs',
-    put_pies_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t30_2\pies',
+    put_backs_here = r'PATH\TO\WHERE\TO\SAVE\DATA\BACKGROUND',
+    put_pies_here = r'PATH\TO\WHERE\TO\SAVE\DATA\CROPPED_IMAGES',
     treshold_in = 200
     )
 elap = count_time.time() - start_time
 print('processing time, s / img')
 print((elap / len(not_done)))
-
-
-# In[]
-
-failed = glob.glob(r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t30_2\pies\failed\*.png')
-
-failed_names = []
-for fn in failed:
-    t1 = (fn.split('\\')[-1][-8:])
-    #failed_names.append(t1)
-    failed_names.append(t1.lower().replace('.png', '.jpg'))
-
-t15_failed = []
-for f in t30[0]:
-    f1 = f.split('\\')
-    f2 = (f1[-1]).lower()
-    for n in failed_names:
-        if n in f2:
-            t15_failed.append(f)
-
-l1 = [
- 'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph3_time30_weight1\\IMG_9150.jpg',
- #'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph3_time30_weight1\\IMG_9173.jpg',
- 'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph3_time30_weight2\\IMG_9015.jpg',
- 'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph3_time30_weight2\\IMG_9152.jpg',
- #'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph3_time30_weight2\\IMG_9175.jpg',
- 'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea70_ph8_time30_weight2\\IMG_8255.jpg',
- #'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea80_ph3_time30_weight1\\IMG_9156.jpg',
- #'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea80_ph3_time30_weight2\\IMG_9158.jpg',
- #'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea80_ph3_time30_weight4\\IMG_9160.jpg',
- #'L:\\Kokini Lab\\Kara Benbow\\pie_crust_pictures_v2\\pea90_ph3_time30_weight4\\IMG_9166.jpg'
-      ]
-
-
-# In[]
-
-# WRITE OUT THE CROPPED IMAGES FOR EASIER FUTURE ANALYSIS. CROPPING IS DONE PER TIME BASED ON THRESHOLD VALUES
-write_out_backs_pies(
-    imgs_to_grab = l1,
-    put_backs_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t30_2\backs',
-    put_pies_here = r'L:\Kokini Lab\Kara Benbow\project_main_v2\crops\t30_2\pies',
-    treshold_in = 500
-    )
-
 
 
 
